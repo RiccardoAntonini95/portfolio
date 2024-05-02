211935a1f8eb16946a2589a1a693bf7b0a0ed6b2
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-05-02 13:18:18 +0200

    setup sidebar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,22 +1,27 @@
 import "./navbar.scss"
 import githubLogo from '/src/assets/img/githubLogo.png'
 import linkedinLogo from '/src/assets/img/linkedinLogo.png'
+import SideBar from "../sidebar/SideBar"
+import { motion } from "framer-motion"
 
 const NavBar = () => {
-    return (
-      <>
-        <div className="navbar">
-            {/* sidebar */}
-            <div className="wrapper">
-                <span>Antonini Riccardo</span>
-                <div className="social">
-                    <a href="#"><img src={githubLogo} alt="GitHub Logo" /></a>
-                    <a href="#"><img src={linkedinLogo} alt="LinkedIn Logo" /></a>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="navbar">
+        <SideBar />
+        <div className="wrapper">
+          <span>Antonini Riccardo</span>
+          <div className="social">
+            <a href="#">
+              <img src={githubLogo} alt="GitHub Logo" />
+            </a>
+            <a href="#">
+              <img src={linkedinLogo} alt="LinkedIn Logo" />
+            </a>
+          </div>
         </div>
-      </>
-    );
-
+      </div>
+    </>
+  );
 }
 export default NavBar
