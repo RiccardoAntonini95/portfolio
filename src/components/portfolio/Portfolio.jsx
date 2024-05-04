b1c4c6b600458a540c79c758ff1a9c512ac574ed
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-05-04 15:26:21 +0200

    projects section desktop

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard"
 const Portfolio = () => {
     return(
         <>
-        <h2 className="portfolioTitle">Some of my projects</h2>
+        <h2 className="portfolioTitle">SELECTED PROJECTS</h2>
         <div className="projectsContainer">
            <ProjectCard projectName={"Pixel Gem Shop"} projectLink={"https://github.com/RiccardoAntonini95/PixelGemShop"} projectPicture={"pixelgem.png"}  />
            <ProjectCard projectName={"In Forno Pizzeria"} projectLink={"https://github.com/RiccardoAntonini95/s19l1-2-3-4-5"} projectPicture={"Inforno.png"}  />
