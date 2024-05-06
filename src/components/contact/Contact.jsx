c027b1121ba99759ca6445dd5483558652b4ddf7
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-05-06 12:01:06 +0200

    links + fix sidebar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,16 +9,13 @@ const Contact = () => {
             <h3>CONTACT ME!</h3>
             <h4>MAIL</h4>
             <a href="mailto:riccardo.antonini95@gmail.com?subject=object&body=text">riccardo.antonini95@gmail.com</a>
-            <h4>PHONE</h4>
-            <p>+39/3465749820</p>
             <h4>SOCIAL</h4>
-            <a href="#">
+            <a href="https://github.com/RiccardoAntonini95" target="_blank" rel="external" >
               <img src={githubLogo} alt="GitHub Logo" />
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/riccardo-antonini-b477212bb/" target="_blank" rel="external" >
               <img src={linkedinLogo} alt="LinkedIn Logo" />
             </a>
-
         </div>
         </>
     )
--- src/components/portfolio/ProjectCard.jsx
+++ src/components/portfolio/ProjectCard.jsx
@@ -3,7 +3,7 @@ import githubLogo from "/src/assets/img/githubLogo.png";
 const ProjectCard = (props) => {
     return(
         <div className="projectCard">
-            <h3>{props.projectName}</h3>
+            <h4>{props.projectName}</h4>
             <img src={`/src/assets/img/${props.projectPicture}`} alt="project-pic" className="projectPic" />
             <a href={props.projectLink} rel="external" target="_blank">See it on GitHub <img src={githubLogo} width={20} alt="GitHub Logo" /></a>
         </div>
